d9a90d981d8b85eb918947f3bc0a5179bea97451
Nicolas <lalobonza@live.com>
2024-11-05 04:38:31 -0300

    Creacion sp insertar empleado con sucursal por ciudad/direccion, e insertar empleado con sucursal por id. Ligeros ajustes al sp insertar sucursal. Agregado casos de prueba para ambos sp

--- SQL Server Scripts1/03 - Script_SP_Insert.sql
+++ SQL Server Scripts1/03 - Script_SP_Insert.sql
@@ -8,20 +8,27 @@ create or alter proc sucursales.insertarSucursal
 	@telefono char(9)
 as
 begin
-	declare @error varchar(500)
-	set @error = ''
-
-	if @ciudad is null or ltrim(rtrim(@ciudad)) = ''
+	--Nico: hice que el resultado de trimmear los datos de entrada se guarden en variables, las validaciones funcionan igual
+	--solo que ahora en la tabla se insertan trimmeadas (antes podias insertar algo como '   La matanza  ' en ciudad y hubiera sido valido por ej.
+	declare @error varchar(500)=''
+	declare @ciudadTrim varchar(20)=''
+	declare @dirTrim varchar(100)=''
+	declare @horarioTrim varchar(50)=''
+	
+	set @ciudadTrim=ltrim(rtrim(@ciudad))
+	if @ciudad is null or @ciudadTrim = ''
 	begin
 		set @error = 'No paso el nombre de la sucursal o paso un nombre vacio' + char(13) + char(10)
 	end
 
-	if @dir is null or ltrim(rtrim(@dir)) = ''
+	set @dirTrim=ltrim(rtrim(@dir))
+	if @dir is null or @dirTrim = ''
 	begin
 		set @error = @error + 'No paso la direccion de la sucursal o paso una direccion vacia' + char(13) + char(10)
 	end
-
-	if @horario is null or ltrim(rtrim(@horario)) = ''
+	
+	set @horarioTrim=ltrim(rtrim(@horario))
+	if @horario is null or @horarioTrim = ''
 	begin
 		set @error = @error + 'No paso el horario de la sucursal o paso un horario vacio' + char(13) + char(10)
 	end
@@ -33,10 +40,10 @@ begin
 
 	if @telefono not like ('[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]')
 	begin
-		set @error = @error + 'El telefono ingresado no es valido, formato te telefono: xxxx-xxxx' + char(13) + char(10)
+		set @error = @error + 'El telefono ingresado no es valido, formato de telefono: xxxx-xxxx' + char(13) + char(10)
 	end
 
-	if exists (select 1 from sucursales.Sucursal where direccion = @dir and ciudad = @ciudad)
+	if exists (select 1 from sucursales.Sucursal where (direccion = @dir and ciudad = @ciudad) or (direccion=@dirTrim and ciudad=@ciudadTrim))
 	begin
 		set @error = @error + 'Ya existe una sucursal en: ' + @dir + char(13) + char(10)
 	end
@@ -48,10 +55,167 @@ begin
 	end
 	
 	insert into sucursales.Sucursal(ciudad, direccion, horario, telefono)
-	values(@ciudad, @dir, @horario, @telefono)
-
+	values(@ciudadTrim, @dirTrim, @horarioTrim, @telefono)
+end
+select * from sucursales.sucursal
+--EMPLEADO
+--SP para ingresar empleado en una sucursal donde la sucursal se determina por ciudad y direccion de la misma
+create or alter procedure recursosHumanos.insertarEmpleadoSucursalPorCiudadDireccion
+@legajo int,
+@nombre varchar(50),
+@apellido varchar(50),
+@dni int,
+@direccionEmp varchar(100),
+@emailPer varchar(60),
+@emailEmp varchar(60),
+@cuil char(13),
+@cargo varchar(20),
+@turno varchar(20),
+@ciudadSuc varchar(20),
+@direccionSuc varchar(100)
+as
+begin
+	declare @error varchar(max)=''
+	declare @idSuc int
+	declare @idCargo int
+	--Validaciones criticas (campos que no pueden ser nulos: legajo, dni, emailPer, emailEmp, cargo, ciudadSuc, direccionSuc, turno)
+	if @legajo is null or @dni is null or @emailPer is null or @emailEmp is null or @cargo is null or @ciudadSuc is null or @direccionSuc is null or @turno is null
+	begin
+		raiserror('Como minimo, un registro de empleado debe tener un legajo, dni, email personal, email empresarial, cargo, ciudad y direccion de sucursal y un turno. Ingrese estos 
+		datos e intente nuevamente',16,1)
+		return
+	end
+	--Validaciones generales
+	--Validacion de legajo unico
+	if (select 1 from recursosHumanos.Empleado where legajo=@legajo) is not null
+		set @error=@error+'Ya existe un empleado con legajo '+cast(@legajo as varchar)+ CHAR(13)+CHAR(10)
+	--Validacion de DNI unico
+	if (select 1 from recursosHumanos.Empleado where dni=@dni) is not null
+		set @error=@error+'Ya existe un empleado con DNI '+ cast(@dni as varchar)+ CHAR(13)+CHAR(10)
+	--Validacion de formato de correo correcto, que no sean iguales entre si y que sean unicos ambos
+	set @emailPer=LTRIM(RTRIM(@emailPer))
+	set @emailEmp=LTRIM(RTRIM(@emailEmp))
+	if(@emailPer=@emailEmp)
+		set @error=@error+'El correo personal es igual que el correo empresarial, deben ser diferentes.'+CHAR(13)+CHAR(10)
+	if(CHARINDEX('@',@emailPer)=0)
+		set @error=@error+'El correo personal ingresado no tiene formato de correo (no contiene un @)'+CHAR(13)+CHAR(10)
+	if(CHARINDEX('@',@emailEmp)=0)
+		set @error=@error+'El correo empresarial ingresado no tiene formato de correo (no contiene un @)'+CHAR(13)+CHAR(10)
+	if(select 1 from recursosHumanos.Empleado where emailEmp=@emailEmp) is not null
+		set @error=@error+'El correo empresarial '+@emailEmp+' ya se encuentra en uso.'+CHAR(13)+CHAR(10)
+	if(select 1 from recursosHumanos.Empleado where emailPer=@emailPer) is not null
+		set @error=@error+'El correo personal '+@emailPer+' ya se encuentra en uso.'+CHAR(13)+CHAR(10)
+	--Validacion de que el cargo existe, obtencion de su id
+	set @cargo=LTRIM(RTRIM(@cargo))
+	set @idCargo=(select id from recursosHumanos.Cargo where cargo=@cargo)
+	if @idCargo is null
+		set @error=@error+'El cargo '+@cargo+' no existe, ingrese el cargo usando recursosHumanos.insertarCargo y luego cargue al empleado'+CHAR(13)+CHAR(10)
+	--Validacion del turno 
+	set @turno=LTRIM(RTRIM(@turno))
+	if @turno not in ('TM', 'TT', 'Jornada Completa')
+		set @error=@error+'El turno ingresado no es valido, este debe ser TM, TT o Jornada Completa'+CHAR(13)+CHAR(10)
+	--Validacion de que la ciudad y direccion ingresadas correspondan a una sucursal, obtencion de ID sucursal
+	set @ciudadSuc=LTRIM(rtrim(@ciudadSuc))
+	set @direccionSuc=LTRIM(RTRIM(@direccionSuc))
+	set @idSuc=(select id from sucursales.Sucursal where ciudad=@ciudadSuc and direccion=@direccionSuc)
+	if @idSuc is null
+		set @error=@error+'No se encontro la sucursal ingresada, asegurese de que esta existe y sino carguela usando sucursales.insertarSucursal y luego cargue al empleado'+CHAR(13)+CHAR(10)
+	--Validacion de formato de cuil y si este fue ingresado como '' o con espacios asignarle null
+	set @cuil=LTRIM(rtrim(@cuil))
+	if @cuil=''
+		set @cuil=null
+	if @cuil is not null
+	begin
+		if @cuil not like ('[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9]')
+			set @error=@error+'Formato de cuil ingresado incorrecto, debe ser xx-xxxxxxxx-x'+CHAR(13)+CHAR(10)
+	end
+	if @error <> ''
+	begin
+		raiserror(@error, 16, 1)
+		return
+	end
+	--Insercion
+	insert into recursosHumanos.Empleado (legajo,nombre,apellido,dni,direccion,emailPer,emailEmp,cuil,cargo,idSucursal,turno,activo)
+	values(@legajo,@nombre,@apellido,@dni,RTRIM(ltrim(@direccionEmp)),@emailPer,@emailEmp,@cuil,@idCargo,@idSuc,@turno,1)
 end
 
+--SP para ingresar empleado en una sucursal donde la sucursal se determina por ID
+create or alter procedure recursosHumanos.insertarEmpleadoSucursalPorId
+@legajo int,
+@nombre varchar(50),
+@apellido varchar(50),
+@dni int,
+@direccionEmp varchar(100),
+@emailPer varchar(60),
+@emailEmp varchar(60),
+@cuil char(13),
+@cargo varchar(20),
+@turno varchar(20),
+@idSucursal int
+as
+begin
+	declare @error varchar(max)=''
+	declare @idCargo int
+	--Validaciones criticas (campos que no pueden ser nulos: legajo, dni, emailPer, emailEmp, cargo, idSucursal, turno)
+	if @legajo is null or @dni is null or @emailPer is null or @emailEmp is null or @cargo is null or @idSucursal is null or @turno is null
+	begin
+		raiserror('Como minimo, un registro de empleado debe tener un legajo, dni, email personal, email empresarial, cargo y un Id de sucursal valido. Ingrese estos 
+		datos e intente nuevamente',16,1)
+		return
+	end
+	--Validaciones generales
+	--Validacion de legajo unico
+	if (select 1 from recursosHumanos.Empleado where legajo=@legajo) is not null
+		set @error=@error+'Ya existe un empleado con legajo '+cast(@legajo as varchar)+ CHAR(13)+CHAR(10)
+	--Validacion de DNI unico
+	if (select 1 from recursosHumanos.Empleado where dni=@dni) is not null
+		set @error=@error+'Ya existe un empleado con DNI '+ cast(@dni as varchar)+ CHAR(13)+CHAR(10)
+	--Validacion de formato de correo correcto, que no sean iguales entre si y que sean unicos ambos
+	set @emailPer=LTRIM(RTRIM(@emailPer))
+	set @emailEmp=LTRIM(RTRIM(@emailEmp))
+	if(@emailPer=@emailEmp)
+		set @error=@error+'El correo personal es igual que el correo empresarial, deben ser diferentes.'+CHAR(13)+CHAR(10)
+	if(CHARINDEX('@',@emailPer)=0)
+		set @error=@error+'El correo personal ingresado no tiene formato de correo (no contiene un @)'+CHAR(13)+CHAR(10)
+	if(CHARINDEX('@',@emailEmp)=0)
+		set @error=@error+'El correo empresarial ingresado no tiene formato de correo (no contiene un @)'+CHAR(13)+CHAR(10)
+	if(select 1 from recursosHumanos.Empleado where emailEmp=@emailEmp) is not null
+		set @error=@error+'El correo empresarial '+@emailEmp+' ya se encuentra en uso.'+CHAR(13)+CHAR(10)
+	if(select 1 from recursosHumanos.Empleado where emailPer=@emailPer) is not null
+		set @error=@error+'El correo personal '+@emailPer+' ya se encuentra en uso.'+CHAR(13)+CHAR(10)
+	--Validacion de que el cargo existe, obtencion de su id
+	set @cargo=LTRIM(RTRIM(@cargo))
+	set @idCargo=(select id from recursosHumanos.Cargo where cargo=@cargo)
+	if @idCargo is null
+		set @error=@error+'El cargo '+@cargo+' no existe, ingrese el cargo usando recursosHumanos.insertarCargo y luego cargue al empleado'+CHAR(13)+CHAR(10)
+	--Validacion del turno 
+	set @turno=LTRIM(RTRIM(@turno))
+	if @turno not in ('TM', 'TT', 'Jornada Completa')
+		set @error=@error+'El turno ingresado no es valido, este debe ser TM, TT o Jornada Completa'+CHAR(13)+CHAR(10)
+	--Validacion de que la ciudad y direccion ingresadas correspondan a una sucursal, obtencion de ID sucursal
+	if not exists(select 1 from sucursales.Sucursal where id=@idSucursal)
+		set @error=@error+'No se encontro la sucursal ingresada, asegurese de que el ID ingresado corresponde con alguna sucursal.'+CHAR(13)+CHAR(10)
+	--Validacion de formato de cuil y si este fue ingresado como '' o con espacios asignarle null
+	set @cuil=LTRIM(rtrim(@cuil))
+	if @cuil=''
+		set @cuil=null
+	if @cuil is not null
+	begin
+		if @cuil not like ('[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9]')
+			set @error=@error+'Formato de cuil ingresado incorrecto, debe ser xx-xxxxxxxx-x'+CHAR(13)+CHAR(10)
+	end
+	if @error <> ''
+	begin
+		raiserror(@error, 16, 1)
+		return
+	end
+	--Insercion
+	insert into recursosHumanos.Empleado (legajo,nombre,apellido,dni,direccion,emailPer,emailEmp,cuil,cargo,idSucursal,turno,activo)
+	values(@legajo,@nombre,@apellido,@dni,RTRIM(ltrim(@direccionEmp)),@emailPer,@emailEmp,@cuil,@idCargo,@idSucursal,@turno,1)
+end
+select * from recursosHumanos.Empleado
+select * from recursosHumanos.cargo
+select * from sucursales.Sucursal
 --EMPLEADO
 --CARGO
 --LINEAPRODUCTO
--- SQL Server Scripts1/12 - Test_Insert.sql
+++ SQL Server Scripts1/12 - Test_Insert.sql
@@ -14,4 +14,50 @@ exec sucursales.insertarSucursal 'San Justo', 'Av. Brig. Gral. Juan Manuel de Ro
 exec sucursales.insertarSucursal 'La Matanza', 'Av. Brig. Gral. Juan Manuel de Rosas 3634, B1754 San Justo, Provincia de Buenos Aires', 'L a V: 8 - 20, S y D: 10 - 20', '1111-1111'
 select * from sucursales.Sucursal
 
+
 --***EMPLEADO***
+--SP para ingresar empleado en una sucursal donde la sucursal se determina por ciudad y direccion de la misma
+--Probamos insertando un empleado que falle las validaciones criticas (como minimo el empleado
+--debe tener un registro de empleado debe tener un legajo, dni, email personal, email empresarial, cargo, ciudad y direccion de sucursal y un turno).
+exec recursosHumanos.insertarEmpleadoSucursalPorCiudadDireccion NULL,'Pollito', 'Perez', 41714473, 'Luisito 298','camilotierra@gmail.com', 'camilotierra@gmail.com2', 20, NULL, NULL, NULL, NULL 
+--Ahora intentemos ingresar un empleado que falle las verificaciones generales:
+/*Validaciones generales:
+legajo unico
+dni unico
+formato de correo, que no sean iguales entre si, y que sean unicos ambos
+que el cargo ingresado exista
+que el turno sea valido
+que la ciudad y direccion ingresadas correspondan con una sucursal
+que el cuil tenga el formato correspondiente (solo si no es nulo)*/
+exec recursosHumanos.insertarEmpleadoSucursalPorCiudadDireccion 257020, 'Pollito', 'Perez', 36383025, 'Av. Vergara 1910 , Hurlingham , Buenos Aires','pollitoPerez@gmail.com','pollitoPerez@gmail.com', 20349,'Barrendero', 'Turno madrugada', 'La matanza','Avenida siempre viva 239' 
+
+--Ahora intentemos con un registro valido
+exec recursosHumanos.insertarEmpleadoSucursalPorCiudadDireccion 300000, 'Pollito', 'Perez', 41714474, 'Av. Vergara 1910 , Hurlingham , Buenos Aires','pollitoPerez@gmail.com','pollitoPEREZ@superA.com','21-41714474-1','Cajero', 'TM', 'San Justo','Av. Brig. Gral. Juan Manuel de Rosas 3634, B1754 San Justo, Provincia de Buenos Aires'
+--Comprobamos que efectivamente se pudo ingresar
+select * from recursosHumanos.empleado
+where legajo=300000 
+
+--borramos el registro
+delete from recursosHumanos.Empleado
+where legajo=300000
+
+--SP para ingresar empleado en una sucursal donde la sucursal se determina por ID
+--Funciona igual que el anterior solo que en vez de la ciudad y direccion de la sucursal este se determina con el ID, las validaciones
+--son iguales salvo que ahora se verifica que el id ingresado corresponda con una sucursal y que no sea null
+
+--intentamos fallar las verificaciones criticas
+exec recursosHumanos.insertarEmpleadoSucursalPorId NULL,'Pollito', 'Perez', 41714473, 'Luisito 298','camilotierra@gmail.com', 'camilotierra@gmail.com2', 20, NULL, NULL, NULL
+
+--intentamos fallar las verificaciones generales
+exec recursosHumanos.insertarEmpleadoSucursalPorId 257020, 'Pollito', 'Perez', 36383025, 'Av. Vergara 1910 , Hurlingham , Buenos Aires','pollitoPerez@gmail.com','pollitoPerez@gmail.com', 20349,'Barrendero', 'Turno madrugada', 900
+
+--Ingresamos un registro valido
+exec recursosHumanos.insertarEmpleadoSucursalPorId 300000, 'Pollito', 'Perez', 41714474, 'Av. Vergara 1910 , Hurlingham , Buenos Aires','pollitoPerez@gmail.com','pollitoPEREZ@superA.com','21-41714474-1','Cajero', 'TM', 1
+--Comprobamos que efectivamente se pudo ingresar
+select * from recursosHumanos.empleado
+where legajo=300000 
+
+--borramos el registro
+delete from recursosHumanos.Empleado
+where legajo=300000
+
